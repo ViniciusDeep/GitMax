b6c2e5cb2a666cbb0a59fb5752df23e46236e22e
Jefferson Figueiredo <33241695+JeffFigueira@users.noreply.github.com>
2020-10-01 11:43:32 -0300

    Update InsideUserTests.swift

--- GitMax/ios-recruitmentTests/Features/InsideUser/InsideUserTests.swift
+++ GitMax/ios-recruitmentTests/Features/InsideUser/InsideUserTests.swift
@@ -18,7 +18,6 @@ class InsideUserTests: XCTestCase {
     let insideUserViewModelStub: InsideUserViewModel = InsideUserViewModel(user: User())
     var insideUserRepoCellViewModelStub: InsideUserRepoCellViewModel?
     
-    
     func testRequestReponse() {
         let jsonStub = [
                    "login" : "mojombo",
@@ -77,15 +76,60 @@ class InsideUserTests: XCTestCase {
         XCTAssertEqual(repositorieStub.starsCount, insideUserRepoCellViewModelStub?.repo?.starsCount)
         XCTAssertEqual(repositorieStub.description, insideUserRepoCellViewModelStub?.repo?.description)
         
-        
-        
         insideUserViewModelStub.repoCellViewModels.append(insideUserRepoCellViewModelStub ?? InsideUserRepoCellViewModel(Repositorie()))
-        
-        
         
         XCTAssertEqual(insideUserViewModelStub.cellViewModel(forIndex: 0).repo?.name, insideUserRepoCellViewModelStub?.repo?.name)
         
-        
         XCTAssertEqual(insideUserViewModelStub.numberOfRepos(), 1)
     }
+    // Unit test to request
+    func testGeneralRequestMethod() {
+         Providey<[User]>().request(router: .users, withMethod: .get, params: nil) { (result) in
+            switch result {
+            case .success(let users):
+                guard let loginAsString = jsonStub["login"] as? String else { XCTAssertTrue(false)
+                                   return}
+                
+                guard let urlAsString = jsonStub["url"] as? String else {XCTAssertTrue(false)
+                    return}
+                
+                guard let repoUrlAsString = jsonStub["repos_url"] as? String else {XCTAssertTrue(false)
+                    return}
+                
+                XCTAssertEqual(users[0].userName, loginAsString)
+                XCTAssertEqual(users[0].userUrl, urlAsString)
+                XCTAssertEqual(users[0].reposUrl, repoUrlAsString)
+                expectationResponse.fulfill()
+                
+                case .failure(let error):
+                      print(error)
+                      XCTAssertNotNil(error)
+                
+            }
+    } 
+    // Test future response
+    func testGeneralResponse() {
+         Providey<[User]>().request(router: .users, withMethod: .get, params: nil) { (result) in
+            switch result {
+            case .success(let users):
+                guard let loginAsString = jsonStub["login"] as? String else { XCTAssertTrue(false)
+                                   return}
+                
+                guard let urlAsString = jsonStub["url"] as? String else {XCTAssertTrue(false)
+                    return}
+                
+                guard let repoUrlAsString = jsonStub["repos_url"] as? String else {XCTAssertTrue(false)
+                    return}
+                
+                XCTAssertEqual(users[0].userName, loginAsString)
+                XCTAssertEqual(users[0].userUrl, urlAsString)
+                XCTAssertEqual(users[0].reposUrl, repoUrlAsString)
+                expectationResponse.fulfill()
+                
+                case .failure(let error):
+                      print(error)
+                      XCTAssertNotNil(error)
+                
+            }
+    }        
 }
